34e3411c0ca1d1f8653e605b48841938c600c93a
guilhermefloriani <floriani.guilherme@gmail.com>
2016-05-09 14:08:16 -0300

    Home boxes

--- web/static/js/containers/home.js
+++ web/static/js/containers/home.js
@@ -22,8 +22,59 @@ export default class Home extends Component {
     )
   }
 
+  _renderGenresBox() {
+    return (
+      <div className="box">
+        <div className="box-header">
+          <span className="box-title">Genres</span>
+        </div>
+        <div className="box-body">
+          teste
+        </div>
+      </div>
+    )
+  }
+
+  _renderListsBox() {
+    return (
+      <div className="box">
+        <div className="box-header">
+          <span className="box-title">Top Lists</span>
+        </div>
+        <div className="box-body">
+          teste
+        </div>
+      </div>
+    )
+  }
+
+  _renderTopMusics() {
+    return (
+      <div className="box">
+        <div className="box-header">
+          <span className="box-title">Top Musics</span>
+        </div>
+        <div className="box-body">
+          teste
+        </div>
+      </div>
+    )
+  }
+
+  _renderNewsBox() {
+    return (
+      <div className="box">
+        <div className="box-header">
+          <span className="box-title">News</span>
+        </div>
+        <div className="box-body">
+          teste
+        </div>
+      </div>
+    )
+  }
+
   render() {
-    // let { currentUser } = this.props;
     return (
       <div className="page">
         <div className="page-content">
@@ -31,6 +82,22 @@ export default class Home extends Component {
             <div className="columns">
               <div className="column is-12">
                 {this._renderSearchBox()}
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-half-desktop">
+                {this._renderGenresBox()}
+              </div>
+              <div className="column is-half-desktop">
+                {this._renderListsBox()}
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-half-desktop">
+                {this._renderTopMusics()}
+              </div>
+              <div className="column is-half-desktop">
+                {this._renderNewsBox()}
               </div>
             </div>
           </div>
